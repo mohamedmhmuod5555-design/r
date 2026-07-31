e8fbe3cce374d30575591d25c9aa55cb255c8eda
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-31 04:03:21 -0700

    Update the best thing 6.py

--- the best thing 6.py
+++ the best thing 6.py
@@ -31,7 +31,7 @@ if sign=='*':
  sc=num1*num2
 if sign=='/':
  sc=num1//num2 
-st.title("Welcome to Mohamed's game ")
+st.title("Welcome to Math Game ")
 st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
 if st.button("تأكيد التخمين "):
